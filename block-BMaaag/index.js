38b2816932c2573f9030ac0cd7c73b6ae5924922
PujaGKundu <pujakundu100@gmail.com>
2022-01-27 19:09:25 +0530

    MongoDB CRUD operations

---
+++ block-BMaaag/index.js
@@ -0,0 +1,18 @@
+var mount = [
+  {
+    name: "xyz",
+    height: "4000 mtrs",
+  },
+  {
+    name: "abc",
+    height: "4000 mtrs",
+  },
+  {
+    name: "pqr",
+    height: "4000 mtrs",
+  },
+  {
+    name: "efg",
+    height: "4000 mtrs",
+  },
+];
